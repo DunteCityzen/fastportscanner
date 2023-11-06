2f74eec1736b9fd3d309a0e43d11b1afe5e54772
Duncan Ochieng <65547730+DunteCityzen@users.noreply.github.com>
2023-11-07 00:11:10 +0300

    Add files via upload

---
+++ socketconnector.py
@@ -0,0 +1,13 @@
+#!bin/python3
+
+import socket
+
+data = 'Hello world'
+ip = '192.168.1.103'
+port = 4444
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((ip, port))
+s.send(data.encode())
+s.close()
+print("Connection successful")
